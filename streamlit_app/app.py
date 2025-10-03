7eb7627457df8528327b1fd2d88010c36b0cc55d
rashedsumon <rashedsumon57@gmail.com>
2025-10-03 11:34:06 +0600

    Update app.py

---
+++ streamlit_app/app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+from excel_helpers import load_template, save_to_excel
+
+st.set_page_config(page_title="Grocery Flyer QA", layout="wide")
+st.title("Grocery Flyer QA & Export")
+
+# File selector for CSV/JSON produced by Make.com (or fetch directly from Drive via API)
+uploaded = st.file_uploader("Upload parse JSON or CSV file from Make.com", type=["json","csv"])
+if uploaded:
+    if uploaded.type.endswith("json"):
+        df = pd.read_json(uploaded)
+    else:
+        df = pd.read_csv(uploaded)
+    edited = st.experimental_data_editor(df, num_rows="dynamic")
+    st.download_button("Export to Excel", data=save_to_excel(edited), file_name="final_output.xlsx")
