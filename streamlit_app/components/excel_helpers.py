e0b8ab60ed33a17c4f9917d349c31d89c2f32278
rashedsumon <rashedsumon57@gmail.com>
2025-10-03 12:06:55 +0600

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch main # Your branch is up to date with 'origin/main'.

--- streamlit_app/components/excel_helpers.py
+++
@@ -1,9 +0,0 @@
-import io
-import pandas as pd
-
-def save_to_excel(df: pd.DataFrame) -> bytes:
-    buffer = io.BytesIO()
-    with pd.ExcelWriter(buffer, engine="openpyxl") as writer:
-        df.to_excel(writer, index=False, sheet_name="Sheet1")
-    buffer.seek(0)
-    return buffer.read()
